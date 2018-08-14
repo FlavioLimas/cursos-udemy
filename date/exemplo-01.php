639d2e1b63b4fac81d6d2b948b28ed6f78662359
Flavio Lima <flaviolima.s@live.com>
2018-08-14 10:12:53 -0300

    date/exemplo-01.php

--- date/exemplo-01.php
+++ date/exemplo-01.php
@@ -1,5 +1,12 @@
 <?php 
-	
-	echo date("d/m/Y H:i:s");
+
+/**
+ * A função abaixo retorna uma data baseada no times temp passado como segundo argumento
+ */
+echo date("d/m/Y H:i:s", 1534251167);
+echo "<br>";
+
+echo time();
+
 
  ?>
