9f65bc7cd89b479595377fd702e91abe467b3fe1
Flavio Lima <flaviolima.s@live.com>
2018-08-31 17:09:36 -0300

    Implementado comando unlink que deleta arquivos e diretorios

--- unlink/exemplo-02.php
+++ unlink/exemplo-02.php
@@ -1,8 +1,16 @@
 <?php 
+// Excluindo arquivos
+if(!is_dir("images")) mkdir("images");
 
-mkdir("images");
+foreach (scandir("images") as $item) {
+	// Ele só entra no if se não tiver no array . ou ..
+	if(!in_array($item,array(".",".."))){
 
+		unlink("images/" . $item);
+	}
+}
 
+echo "Deletado arquivos";
 
 
  ?>
