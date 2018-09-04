6143a89bb933148ff5c4182d0eb4992f95d6f41a
Flavio Lima <flaviolima.s@live.com>
2018-09-04 15:06:59 -0300

    Trabalhando com a biblioteca CURL (API) REF http://php.net/manual/pt_BR/book.curl.php

--- curl/exemplo-01.php
+++ curl/exemplo-01.php
@@ -1,23 +1,24 @@
 <?php 
+
 // Trabalhando com a biblioteca CURL REF http://php.net/manual/pt_BR/book.curl.php
-$cep = "031310100";
+$cep = "01310100";
 
 $link = "https://viacep.com.br/ws/$cep/json/";
 
 // Inicia o cURL
-$curl = curl_init($link);
+$ch = curl_init($link);
 
 // Informa que a implementação terá um retorno
-curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 
 // Valição de SSL para saber se é valido ou não
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
 
 // pegando o retorno da execução
-$response = curl_exec($curl);
+$response = curl_exec($ch);
 
 // fechando a conexão
-curl_close($curl);
+curl_close($ch);
 
 // Decodificando para array passando o true no segundo parametro
 $data = json_decode($response, true);
