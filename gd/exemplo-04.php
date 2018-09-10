492d86f0b9edf054a8da3b595e240be2e2c4f4ca
Flavio Lima <flaviolima.s@live.com>
2018-09-10 17:16:18 -0300

    Templete de geração de boletos Github https://github.com/CobreGratis/boletophp

--- gd/exemplo-04.php
+++ gd/exemplo-04.php
@@ -4,6 +4,8 @@ header("content-type: image/jpeg");
 
 $file = "wallpaper.jpg";
 
+
+
 $new_width = 256;
 $new_height = 256;
 
@@ -19,7 +21,7 @@ list($old_width, $old_heigth) = getimagesize($file);
 $new_image = imagecreatetruecolor($new_width,$new_height);
 $old_image = imagecreatefromjpeg($file);
 
-// Função que permite criar thumbnail e também permite fazer o crop ou resize da imagem
+// Função que permite criar thumbnail e também permite fazer o crop ou resize da imagem REF http://php.net/manual/pt_BR/function.imagecopyresampled.php
 imagecopyresampled($new_image, $old_image, 0, 0, 0, 0, $new_width, $new_height, $old_width, $old_heigth);
 
 // Gerar a imagem jpeg
