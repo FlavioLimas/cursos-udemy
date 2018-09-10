ff21798b542720ba5132c45dbd925822ca4911c8
Flavio Lima <flaviolima.s@live.com>
2018-09-10 10:04:28 -0300

    Importando fontes do google web fonts para lib GD e gerando um certificado

--- gd/exemplo-03.php
+++ gd/exemplo-03.php
@@ -8,16 +8,16 @@ $titleColor = imagecolorallocate($image, 0, 0, 0);
 // 
 $gray = imagecolorallocate($image, 100, 100, 100);
 
-imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
-imagestring($image, 5, 440, 350, "Flavio Lima", $titleColor);
+imagettftext($image, 32, 0, 350, 150, $titleColor, "fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf","CERTIFICADO");
+imagettftext($image, 32, 0, 420, 350, $titleColor, "fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf", "Flavio Lima");
 // Converte uma string com caracteres ISO-8859-1 codificadas com UTF-8 para single-byte ISO-8859-1 Deixa caracteres de acenturação legiveis http://php.net/manual/pt_BR/function.utf8-decode.php
-imagestring($image, 3, 415, 390, utf8_decode("Concluído em: ").date("d/m/Y"), $titleColor);
+imagestring($image, 3, 430, 390, utf8_decode("Concluído em: ").date("d/m/Y"), $titleColor);
 
 // Tipo da imagem que vc quer criar
 header("Content-type: image/jpeg");
 
 // Gerando imagem do novo certificado com a data atual
-imagejpeg($image, "certificado-".date("Y-m-d").".jpg",80);
+imagejpeg($image);
 
 imagedestroy($image);
 
