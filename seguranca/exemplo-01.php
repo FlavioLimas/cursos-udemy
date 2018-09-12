a17688608ed282efa865ac9c516cb2d4922ae3c8
Flavio Lima <flaviolima.s@live.com>
2018-09-12 18:15:48 -0300

    ajustando

--- seguranca/exemplo-01.php
+++ seguranca/exemplo-01.php
@@ -1,10 +1,24 @@
 <?php 
+// escapeshellcmd() A função impede a injeção de comandos passados via POST ou GET REF http://php.net/manual/pt_BR/function.escapeshellcmd.php
 
-echo "<pre>";
 
-$comando = system("dir C:", $retorno)
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+	$cmd = escapeshellcmd($_POST["cmd"]);
+	var_dump($cmd);
 
-echo "</pre>";
 
+	echo "<pre>";
+
+	$comando = system($cmd, $retorno);
+
+	echo "</pre>";
+
+}
 
  ?>
+
+
+ <form method="post">
+ 	<input type="text" name="cmd">
+ 	<button type="submit">Enviar</button>
+ </form>
