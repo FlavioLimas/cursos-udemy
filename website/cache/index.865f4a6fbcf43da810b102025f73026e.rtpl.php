2a3c5d1842e82dc79b8f76e81b4b06435206c46d
Flavio Lima <flaviolima.s@live.com>
2018-09-12 15:40:10 -0300

    Trabalhando com rotas com Slim Micro-Framework

---
+++ website/cache/index.865f4a6fbcf43da810b102025f73026e.rtpl.php
@@ -0,0 +1,11 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Home Page</title>
+</head>
+<body>
+	<div>Hello</div>
+	<div>Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Mé faiz elementum girarzis, nisi eros vermeio. In elementis mé pra quem é amistosis quis leo. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.</div>
+</body>
+</html>
--- website/index.php
+++ website/index.php
@@ -1,7 +1,47 @@
 <?php 
 
-phpinfo();
+require_once("vendor/autoload.php");
 
+// Estanciando classe que define Rotas
+$app = new \Slim\Slim();
+
+// Chamando namespece que define os templetes
+// namespace
+use Rain\Tpl;
+// config
+$config = array(
+    "tpl_dir"       => "tpl",
+    "cache_dir"     => "cache/"
+);
+Tpl::configure( $config );
+
+
+
+
+// Pagina inicial
+$app->get('/', function(){
+	// echo "Home Page";
+	/*echo json_encode(array(
+		'date'=>date("Y-m-d H:i:s")	
+		)
+	);*/
+	$tpl = new Tpl;
+	// $tpl->assign( "page2", "page2.html" );
+	// $tpl->assign( "version", PHP_VERSION );
+	// draw the template (Gerando o templete)
+	$tpl->draw( "index" );
+});
+
+// Crinado Rotas
+$app->get('/:name', function ($name) {
+    	/*$tpl = new Tpl;
+    	$tpl->assign("name", $name );
+    	$tpl->draw( "teste.html" );*/
+    	echo "Hello, " . $name;
+	}
+);
+
+$app->run();
 
 
  ?>
