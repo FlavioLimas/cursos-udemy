5cd3c4751614152b9c023eadda5cceb2d82a81f3
Flavio Lima <flaviolima.s@live.com>
2018-09-14 18:03:55 -0300

    adicionado comentario a aula de sessoes

--- ataque-sessoes/exemplo-01.php
+++ ataque-sessoes/exemplo-01.php
@@ -1,5 +1,5 @@
 <?php 
-
+// impedindo ataque a sessoes
 session_start();
 
 // Em toda página onde tiver autenticação, depois de verificar login e senha reiniciar o ID da sessão (gerando um novo ID)
